63464cf0dda8bd7563b1b9cf26ee57fc4ac21e2e
James Broughton <jxb@epdt115.ph.bham.ac.uk>
2014-11-03 16:31:07 +0000

    Updated for exercise 4

--- ex2.cpp
+++ ex2.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <climits>
+
 int main()
 {
   // This is a comment
@@ -7,41 +9,53 @@ int main()
 
   double a=0;
   double b=0;
-  double c=0;
+  char c=0;
   double d=0;
 
+  
+
+  while(std::cin)
+  {
+    std::cout << "Type in a value for variable 1: ";
+    std::cin >> a;
+    if (!std::cin)
+      {
+        std::cout << "You entered it wrong!";
+	std::cin.clear();
+	std::cin.ignore(INT_MAX, '\n');
+	continue;
+      }
+
+    std::cout << std::endl << "Type in a value for variable 2: ";
+    std::cin >> b;
+    if (!std::cin)
+      {
+        std::cout << "You entered it wrong!";
+	std::cin.clear();
+	std::cin.ignore(INT_MAX, '\n');
+	continue;
+      }
+
+    std::cout << "What operation would you like to perform? (a=addition, s=subtraction, m=multiplication and d=division; or q to quit) ";
+    std::cin >> c;
+
+    if(c=='a') d=a+b; 
+    else if(c=='s') d=a-b;
+    else if(c=='m') d=a*b;
+    else if(c=='d') d=a/b;
+    else if(c=='q') break;
+    else
+      {
+        std::cout << "You entered it wrong!" << std::endl;
+	std::cin.clear();
+	std::cin.ignore(INT_MAX, '\n');
+	continue;
+      }
+
+    std::cout << std::endl << "Operation of the two variables: " << d << std::endl;
 
 
-  std::cout << "Type in a value for variable 1: ";
-  std::cin >> a;
- if (!std::cin)
-    {
-       std::cout << "You entered it wrong!";
-       return 1;
-    }
-
-  std::cout << std::endl << "Type in a value for variable 2: ";
-  std::cin >> b;
- if (!std::cin)
-    {
-       std::cout << "You entered it wrong!";
-       return 1;
-    }
-
-  std::cout << "What operation would you like to perform? (1=addition, 2=subtraction, 3=multiplication and 4=division) ";
-  std::cin >> c;
-
-  if (!std::cin)
-    {
-      std::cout << "You entered it wrong!" << std::endl;
-       return 1;
-    }
-  else if(c==1)  d=a+b; 
-  else if(c==2) d=a-b;
-  else if(c==3) d=a*b;
-  else if(c==4) d=a/b;
-
-  std::cout << std::endl << "Operation of the two variables: " << d << std::endl;
-
+    break;
+  }
   return 0;
 }
