c610fed649d1ada1ce5af171f24032684dc1f44a
James Broughton <jxb@epdt115.ph.bham.ac.uk>
2014-11-03 15:44:50 +0000

    Updated for exercise 3

--- ex2.cpp
+++ ex2.cpp
@@ -7,14 +7,41 @@ int main()
 
   double a=0;
   double b=0;
+  double c=0;
+  double d=0;
+
+
+
   std::cout << "Type in a value for variable 1: ";
   std::cin >> a;
-  std::cout << "\n" << "Type in a value for variable 2: ";
+ if (!std::cin)
+    {
+       std::cout << "You entered it wrong!";
+       return 1;
+    }
+
+  std::cout << std::endl << "Type in a value for variable 2: ";
   std::cin >> b;
+ if (!std::cin)
+    {
+       std::cout << "You entered it wrong!";
+       return 1;
+    }
 
-  double c=a*b;
+  std::cout << "What operation would you like to perform? (1=addition, 2=subtraction, 3=multiplication and 4=division) ";
+  std::cin >> c;
 
-  std::cout << "\n" << "Product of the two variables: " << c << "\n";
+  if (!std::cin)
+    {
+      std::cout << "You entered it wrong!" << std::endl;
+       return 1;
+    }
+  else if(c==1)  d=a+b; 
+  else if(c==2) d=a-b;
+  else if(c==3) d=a*b;
+  else if(c==4) d=a/b;
+
+  std::cout << std::endl << "Operation of the two variables: " << d << std::endl;
 
   return 0;
 }
