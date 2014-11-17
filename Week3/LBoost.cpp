9edff1605269be362c74c099ff41ad576f672f72
James Broughton <jxb@eprexb.ph.bham.ac.uk>
2014-11-17 17:15:09 +0000

    Added week3

---
+++ Week3/LBoost.cpp
@@ -0,0 +1,45 @@
+#include "LBoost.hpp"
+
+#include <cmath>
+#include <iostream>
+
+FourVector::FourVector(const double x,const double y,const double z,const double t)
+  :m_x(x),m_y(y),m_z(z),m_t(t)
+{
+  m_interval = interval();
+}
+
+FourVector::FourVector(const FourVector& other)
+  :m_x(other.m_x),m_y(other.m_y),m_z(other.m_z),m_t(other.m_t)
+{
+}
+
+FourVector* FourVector::applyLBoost( double v) const
+{
+  double Gamma = 1/sqrt(1-v*v);
+
+  FourVector* BoostedFourVec = new FourVector(m_x, m_y, Gamma*((m_z) - v*(m_t)),Gamma*((m_t) - v*(m_z)));
+  
+    // BoostedFourVec->m_y = m_y;
+    //BoostedFourVec->m_z = Gamma*((m_z) - v*(m_t));
+    //BoostedFourVec->m_t = Gamma*((m_t) - v*(m_z));
+  return BoostedFourVec;
+}
+
+void FourVector::PrintFourVec()
+{
+  std::cout << "Your new boosted components are..." << std::endl;
+  std::cout << "x: " << m_x << ", y: " << m_y << ", z:" << m_z << ", t:" << m_t << std::endl;
+}
+
+double FourVector::interval() const
+{
+  // Assume natural units
+  double s = m_t*m_t - m_x*m_x - m_y*m_y - m_z*m_z;
+  return s;
+}
+
+double FourVector::VecLength() const
+{
+  return sqrt(m_interval);
+}
---
+++ Week3/LBoost.hpp
@@ -0,0 +1,26 @@
+#ifndef LBOOST_HPP
+#define LBOOST_HPP
+
+class FourVector
+{
+public:
+  //ctors
+  FourVector() { m_x = 0; m_y = 0; m_z = 0; m_t = 0;m_interval = interval();}
+  FourVector(const double x, const double y, const double z, const double t);
+  FourVector(const FourVector& other);
+
+  //Member Functions
+  FourVector* applyLBoost(double v) const;//Takes old FourVector and creates new boosted one - retains old.
+  void PrintFourVec(); //Outputs the four vectors
+  double VecLength() const;
+  double interval() const;
+private:
+  //Member variables
+  double m_x;
+  double m_y;
+  double m_z;
+  double m_t;
+  double m_interval;
+};
+#endif
+// LBOOST_HPP
